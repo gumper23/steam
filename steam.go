b27a088ae4d9c9b3f73e2fb4a239a166090223c3
gumper23 <gumper@gumper.com>
2016-05-12 00:59:43 -0700

    Refactored. Added existence check to prevent gaps in auto increments.

--- steam.go
+++ steam.go
@@ -2,123 +2,154 @@ package main
 
 import (
     "database/sql"
-    "encoding/json"
-    "fmt"
-    _ "github.com/go-sql-driver/mysql"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "sort"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+    "strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type OwnedGamesWithNames struct {
-    Response `json:"response"`
+type OwnedGames struct {
+	Response struct {
+		GameCount int `json:"game_count"`
+		Games     []struct {
+			Appid                    int    `json:"appid"`
+			HasCommunityVisibleStats bool   `json:"has_community_visible_stats"`
+			ImgIconURL               string `json:"img_icon_url"`
+			ImgLogoURL               string `json:"img_logo_url"`
+			Name                     string `json:"name"`
+			PlaytimeForever          int    `json:"playtime_forever"`
+		} `json:"games"`
+	} `json:"response"`
 }
 
-type Response struct {
-    GameCount uint   `json:"game_count"`
-    Games     Games `json:"games"`
-}
-
-type Game struct {
-    Appid                    int    `json:"appid"`
-    HasCommunityVisibleStats bool   `json:"has_community_visible_stats"`
-    ImgIconURL               string `json:"img_icon_url"`
-    ImgLogoURL               string `json:"img_logo_url"`
-    Name                     string `json:"name"`
-    PlaytimeForever          int    `json:"playtime_forever"`
-}
-
-type Games []Game
-
-func (slice Games) Len() int {
-    return len(slice)
-}
-
-func (slice Games) Less(i, j int) bool {
-    return slice[i].PlaytimeForever < slice[j].PlaytimeForever
-}
-
-func (slice Games) Swap(i, j int) {
-    slice[i], slice[j] = slice[j], slice[i]
+type StoredGame struct {
+	Appid           int
+	PlaytimeForever int
 }
 
 func main() {
-    steam_api_key := os.Getenv("STEAM_API_KEY")
-    if steam_api_key == "" {
-        panic("Environment variable STEAM_API_KEY not set")
-    }
-
-    steam_id := os.Getenv("STEAM_ID")
-    if steam_id == "" {
-        panic("Environment variable STEAM_ID not set")
-    }
-
-    url := "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?" +
-           "key=" + steam_api_key +
-           "&steamid=" + steam_id +
-           "&include_appinfo=1" + 
-           "&format=json"
-
-    res, err := http.Get(url)
-    if err != nil {
-        panic(err)
-    }
-    defer res.Body.Close()
-
-    body, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        panic(err)
-    }
-    // fmt.Printf("%s\n\n", body)
-
-    var ogs OwnedGamesWithNames
-    err = json.Unmarshal(body, &ogs)
-    if err != nil {
-        panic(err)
-    }
-    sort.Sort(ogs.Response.Games)
-
-    mysql_username := os.Getenv("MYSQL_USERNAME")
-    if mysql_username == "" {
+    mysqlUsername := os.Getenv("MYSQL_USERNAME")
+    if mysqlUsername == "" {
         panic("Environment variable MYSQL_USERNAME not set")
     }
 
-    mysql_password := os.Getenv("MYSQL_PASSWORD")
-    if mysql_password == "" {
+    mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+    if mysqlPassword == "" {
         panic("Environment variable MYSQL_PASSWORD not set")
     }
 
-    db, err := sql.Open("mysql", 
-                        mysql_username +
+    db, err := sql.Open("mysql",
+                        mysqlUsername +
                         ":" +
-                        mysql_password +
+                        mysqlPassword +
                         "@tcp(:3306)/steam")
     if err != nil {
         panic(err)
     }
     defer db.Close()
 
-    // Get the current timestamp for this run.
-    rows, err := db.Query("select current_timestamp() as created_at_ts")
+    rows, err := db.Query("select current_timestamp() as created")
     if err != nil {
         panic(err)
     }
     defer rows.Close()
 
-    var created_at_ts string
+    var created string
     for rows.Next() {
-        err = rows.Scan(&created_at_ts)
+        err = rows.Scan(&created)
         if err != nil {
             panic(err)
         }
     }
-    if created_at_ts == "" {
-        panic("Unable to get a current timestamp?!")
+    if created == "" {
+        panic("Invalid timestamp?!")
     }
 
-    sql := `
-insert ignore into game
+    rows, err = db.Query("select app_id, playtime_forever from game")
+    if err != nil {
+        panic(err)
+    }
+    defer rows.Close()
+
+    sgs := make(map[int]int)
+    for rows.Next() {
+        var sg StoredGame
+        err = rows.Scan(&sg.Appid, &sg.PlaytimeForever)
+        if err != nil {
+            panic(err)
+        }
+        sgs[sg.Appid] = sg.PlaytimeForever
+    }
+    fmt.Println(sgs)
+
+	steamAPIKey := os.Getenv("STEAM_API_KEY")
+	if steamAPIKey == "" {
+		panic("Environment variable STEAM_API_KEY not set")
+	}
+
+	steamID := os.Getenv("STEAM_ID")
+	if steamID == "" {
+		panic("Environment variable STEAM_ID not set")
+	}
+
+	url := "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
+		steamAPIKey +
+		"&steamid=" +
+		steamID +
+		"&include_appinfo=1" +
+		"&format=json"
+	// fmt.Println(url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(body)
+
+	var ogs OwnedGames
+	err = json.Unmarshal(body, &ogs)
+	if err != nil {
+		panic(err)
+	}
+
+	var total, played, inserted, updated int
+	for i, g := range ogs.Response.Games {
+		total++
+		if g.PlaytimeForever > 0 {
+			played++
+		}
+		fmt.Printf("Game = [%d], Playtime = [%d], Name = [%s]\n",
+			i+1,
+			g.PlaytimeForever,
+			g.Name)
+
+        if playtime, ok := sgs[g.Appid]; ok {
+            if playtime != g.PlaytimeForever {
+                updated++
+                fmt.Println("Updating " + g.Name)
+                _, err = db.Exec("update game set playtime_forever = " +
+                                 strconv.Itoa(g.PlaytimeForever) +
+                                 " where app_id = " +
+                                 strconv.Itoa(g.Appid))
+                if err != nil {
+                    panic(err)
+                }
+            }
+        } else {
+            fmt.Println("Inserting " + g.Name)
+            inserted++
+            ins := `
+insert into game
 (
     app_id
     , has_community_visible_stats
@@ -138,26 +169,25 @@ values
     , ?
     , ?
 )`
+            _, err = db.Exec(ins,
+                             g.Appid,
+                             g.HasCommunityVisibleStats,
+                             g.ImgIconURL,
+                             g.ImgLogoURL,
+                             g.Name,
+                             g.PlaytimeForever,
+                             created)
+            if err != nil {
+                panic(err)
+            }
+        }
+	}
 
-    for i, g := range ogs.Response.Games {
-        fmt.Printf("Game = [%d], Playtime [%d], Name = [%s]\n", 
-                   i + 1, 
-                   g.PlaytimeForever, 
-                   g.Name)
-        _, err := db.Exec(sql, 
-                          g.Appid, 
-                          g.HasCommunityVisibleStats, 
-                          g.ImgIconURL,
-                          g.ImgLogoURL,
-                          g.Name,
-                          g.PlaytimeForever,
-                          created_at_ts)
-        if err != nil {
-            panic(err)
-       }
-    }
+	fmt.Printf("Total games = [%d], Played = [%d], Played %% = [%.2g]\n",
+               total,
+		       played,
+		       float64(played)/float64(total)*100)
+    fmt.Printf("Inserted = [%d], Updated = [%d]\n", inserted, updated)
 
-    // fmt.Printf("%+v\n", ogs)
-    // fmt.Printf("%v\n", ogs.Response.GameCount)
+	fmt.Println("Done!\n")
 }
-
